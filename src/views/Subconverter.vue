0c483a51c3546add41025c8ecdb97841e6c51075
pengfujin-momo <sweeterpeng@gmail.com>
2022-11-03 16:31:37 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,13 +255,13 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        customBackend: {
-          "192.168.123.16（内网)": "http://192.168.123.16:25500/sub?",
-          "subconverter作者提供": "https://sub.xeton.dev/sub?",
-          "sub-web作者提供": "https://api.wcc.best/sub?",
-          "sub作者&lhie1提供": "https://api.dler.io/sub?",
-          "sub.id9.cc (品云提供)": "https://sub.id9.cc/sub?",
-        },
+        // customBackend: {
+        //   "192.168.123.16（内网)": "http://192.168.123.16:25500/sub?",
+        //   "subconverter作者提供": "https://sub.xeton.dev/sub?",
+        //   "sub-web作者提供": "https://api.wcc.best/sub?",
+        //   "sub作者&lhie1提供": "https://api.dler.io/sub?",
+        //   "sub.id9.cc (品云提供)": "https://sub.id9.cc/sub?",
+        // },
         backendOptions: [
           {value: "http://192.168.123.16:25500/sub?" },
           {value: "https://sub.xeton.dev/sub?"},
